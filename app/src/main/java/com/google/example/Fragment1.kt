6b29574916b52afef3ea0a4a15814c86ffe76f1f
Anatoly Ovchinnikov <aovchinnikov@shakuro.com>
2018-09-27 17:13:26 +0300

    Added project

---
+++ app/src/main/java/com/google/example/Fragment1.kt
@@ -0,0 +1,41 @@
+package com.google.example
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment1_layout.*
+
+class Fragment1 : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+
+        return inflater.inflate(R.layout.fragment1_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goToFragment2.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_fragment1_to_fragment2, null))
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() {}
+    }
+}
