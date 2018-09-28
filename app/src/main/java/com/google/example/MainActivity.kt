06feab75931f5f926543b3e596436e1cac4ce736
Anatoly Ovchinnikov <aovchinnikov@shakuro.com>
2018-09-28 17:55:01 +0300

    Added Drawer view

--- app/src/main/java/com/google/example/MainActivity.kt
+++ app/src/main/java/com/google/example/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.google.example
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val navController: NavController = findNavController(R.id.my_nav_host_fragment)
+        navigationView.setupWithNavController(navController)
     }
 }
