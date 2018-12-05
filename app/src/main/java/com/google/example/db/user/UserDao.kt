1bc53e33162c9f94bdb6c0d809ea7f5b9a35f0c1
Anatoly Ovchinnikov <aovchinnikov@shakuro.com>
2018-12-05 13:57:32 +0300

    minor changes

--- app/src/main/java/com/google/example/db/user/UserDao.kt
+++ app/src/main/java/com/google/example/db/user/UserDao.kt
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
+import com.google.example.db.comment.Comment
 
 @Dao
 interface UserDao {
@@ -16,6 +17,9 @@ interface UserDao {
     @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
             "last_name LIKE :last LIMIT 1")
     fun findByName(first: String, last: String): User
+
+    @Query("SELECT * FROM comment WHERE userId = :userId")
+    fun getComments(userId: Int): List<Comment>
 
     @Insert
     fun insertAll(vararg users: User)
