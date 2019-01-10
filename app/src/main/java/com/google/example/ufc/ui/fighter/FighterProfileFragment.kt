5a3514f5ab50f4509f11528a4210ad2a5c3a06c9
Anatoly Ovchinnikov <aovchinnikov@shakuro.com>
2019-01-10 13:08:00 +0300

    Added backdrop

--- app/src/main/java/com/google/example/ufc/ui/fighter/FighterProfileFragment.kt
+++ app/src/main/java/com/google/example/ufc/ui/fighter/FighterProfileFragment.kt
@@ -1,15 +1,18 @@
 package com.google.example.ufc.ui.fighter
 
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.android.codelabs.paging.db.MainDatabase
+import com.google.codelabs.mdc.kotlin.shrine.BackdropIconClickListener
 import com.google.example.R
 import com.google.example.ufc.api.Api
 import com.google.example.ufc.data.FightersRepository
@@ -33,6 +36,18 @@ class FighterProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        context?.let {
+            showBackdrop.setOnClickListener(BackdropIconClickListener(
+                    it,
+                    fighter_details,
+                    AccelerateDecelerateInterpolator()))
+        }
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            fighter_details.background = context?.getDrawable(R.drawable.fighter_details_background_shape)
+        }
 
         val cache = FightersLocalCache(MainDatabase.getInstance(requireContext()).fightersDao(), Executors.newSingleThreadExecutor())
         val repository = FightersRepository(Api.create(), cache)
